78112e1e0e90d1af4b0cd53a061faf3042e62d7d
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-01-07 23:07:52 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-document.querySelector('.container');
+let container = document.querySelector('.container');
 container.innerHTML = `
-   <div class="header"></div>
+   <div  class="header"><h3>Register for</h3>
+<h1>Full Stack Conf</h1></div>
     <div class="form">
         <form>
-            <fieldset style="display:grid; grid-template-columns:1fr;">
+            <fieldset class="basic-info">
                 <legend>Basic Info</legend>
+                
                 <label for="name">Name: </label>
                 <input id="name" class="name" type="text" required />
+            <div class="error hide">please enter an email</div>
                 <label for="email">Email: </label>
                 <input id="email" class="email" type="email" required />
                 <label for="job">Job Role</label>
@@ -24,100 +27,83 @@ container.innerHTML = `
                 </select>
             </fieldset>
             <fieldset class="tshirts">
-
                 <legend>T-Shirt Info</legend>
-                <ul>
-                    <li>
-                        <label for="size">Size:</label>
-                        <select>
-                            <option>S</option>
-                            <option>M</option>
-                            <option>L</option>
-                            <option>XL</option>
-                        </select>
-                    </li>
-                    <li class="design">
-                   <label for="design">Design:  </label>
-                        <select>
-                        <option class="love">Theme - I Love JS</option>
-                        <option class="puns">Theme - JS Puns</option>
-                        </select>
-                    </li>
-                    <li class="pun_colors">
-                        <label for="color">Color:</label>
-                        <select>
-                     <option>Dark Slate Grey(Puns only)</option>
-                     <option>Cornflower Blue(Puns only)</option>
-                     <option>Gold is best(puns only)</option>
-                        </select>
-                    </li>
-                    <li class="love_colors">
-                        <label for="color">Color:</label>
-                        <select>
-                            <option>Dark Slate Grey(Puns only)</option>
-                            <option>Cornflower Blue(Puns only)</option>
-                            <option>Gold is best(puns only)</option>
-
-
-                        </select>
-                    </li>
-                </ul>
-
-
+            
+           <ul>
+            <li>
+            <label for="size">Size:</label>
+                <select>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                </select>
+            </li>
+            <li>
+            <label for="design">Design:  </label>
+            <select>
+            <option class="love">Theme - I Love JS</option>
+            <option class="puns">Theme - JS Puns</option>
+            </select>
+            </li>
+            <li class="color-puns">
+            <label for="color">Color:</label>
+            <select>
+            <option>Dark Slate Grey(Puns only)</option>
+            <option>Cornflower Blue(Puns only)</option>
+            <option>Gold is best(puns only)</option>
+            </select>
+            </li>
+            <li class="color-love hide">
+            <label for="color">Color:</label>
+            <select>
+            <option>Dark Slate Grey(Puns only)</option>
+            <option>Cornflower Blue(Puns only)</option>
+            <option>Gold is best(puns only)</option>
+            </select>
+            </li>
+            </ul>
 
             </fieldset>
             <fieldset class="activities">
                 <legend>Register for Activities</legend>
-                <ul>
-                    <li>
+              
                         <input type="checkbox" class="main" /> <label id="main" for="main">Main Conference - $200</label>
-                    </li>
-                    <li>
                         <input type="checkbox" class="frameworks" /> <label for="frameworks"> JavaScript Frameworks Workshop - $200</label>
 
-                    </li>
-                </ul>
                
             </fieldset>
-            <fieldset class="payment">
+            <fieldset class="payment-info">
                 <legend>Payment Info</legend>
-                <ul>
-                    <li>
                         <label for="paymentType">
                             I'm going to pay with:
                         </label>
-                    </li>
-                    <li>
                         <select>
-
-                            <option>Credit Card</option>
-                            <option>Paypal</option>
-                            <option>Bitcoin</option>
+                      <option>Credit Card</option>
+                      <option>Paypal</option>
+                      <option>Bitcoin</option>
                         </select>
-                    </li>
-                </ul>
-                <ul class="card">
-
-                    <li>
+                     <ul class="card-info">
+                        <li>
                         <label>Card Number:</label>
                         <input type="text" />
-                    </li>
-                    <li>
+
+                        </li>
+                        <li>
                         <label>Zip Code:</label>
                         <input type="text" />
-                    </li>
-                    <li>
+
+                        </li>
+                        <li>
                         <label>CW:</label>
                         <input type="text" />
-                    </li>
-                </ul>
-                <ul>
-                    <li>
+
+                        </li>
+                    </ul>
+                   
                         <label>Expiration Date:</label>
                        
-                    </li>
-                    <li>
-
+              
                         <select>
 
                             <option value="01">January</option>
@@ -133,11 +119,7 @@ container.innerHTML = `
                             <option value="11">November</option>
                             <option value="12">December</option>
                         </select>
-                    </li>
-                    <li>
                         <label>Expiration Year:</label>
-                    </li>
-                    <li>
                   
                         <select name="year">
           <option value="2030">2030</option>
@@ -153,10 +135,7 @@ container.innerHTML = `
           <option value="2020">2020</option>
 
      </select>
-                 
-                    </li>
-                </ul>
-
+              
 
             </fieldset>
         </form>
