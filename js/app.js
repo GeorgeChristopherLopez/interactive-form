4ea2e1275d0837103f74f1d6ccc468cc859eaac2
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-01-10 20:23:32 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,47 +1,97 @@
-let status = false;
-let parent = document.createElement("div");
+let submitted = false;
+
 let submitBtn = document.querySelector('.submit');
-let basicInfo = document.querySelector('.basic-info');
 let email = document.querySelector('.email');
 console.log(email);
-let activities = document.querySelector('.activities');
-let card = document.querySelector('.card-info');
 
+let cardNum = document.querySelector('.cardNum');
+let zipCode = document.querySelector('.zipCode');
+let cvv = document.querySelector('.cvv');
 
-// div element
-let theKid = document.createElement("div");
-theKid.innerHTML = 'Are we there yet?';
+// error messages
+let basicError = document.querySelector('#basic-error');
+let emailError = document.querySelector('#email-error');
+let activitiesError = document.querySelector('#activities-error');
+
+let total = document.querySelector('.total');
+let cardError = document.querySelector('#card-error');
+let zipCodeError = document.querySelector('#zip-code-error');
+let cvvError = document.querySelector('#cvv-error');
+
 //
+email.addEventListener('keyup', e => {
+    // check if email.value contains
+    // @xyz.com 
+    console.log('typing email');
 
-submitBtn.addEventListener('click', e => {
-    if (!status) {
-
-        basicInfo.innerHTML = `<p style="background: red; color: white; padding: 1rem; margin: 1rem;">BASIC ERROR</p>` + basicInfo.innerHTML;
-
-      
-
-        //test
-        console.log(email);
-
-        email.innerHTML=`<div><p style="background: red; color: white; padding: 1rem; margin: 1rem;">EMAIL ERROR</p></div>` + email.innerHTML;
-
-        //test
-        console.log(email);
+});
 
 
-        activities.innerHTML = `<p style="background: red; color: white; padding: 1rem; margin: 1rem;">ACTIVITES ERROR</p>` + activities.innerHTML;
+cardNum.addEventListener('keyup', e => {
+   
+
+});
+
+zipCode.addEventListener('keyup', e => {
+    
+  
+
+});
 
 
-        card.innerHTML = `<p style="background: red; color: white; padding: 1rem; margin: 1rem; display: grid; grid-template-columns: 1fr;">CREDIT CARD ERROR</p>` + card.innerHTML;
-
-     
-    }
+cvv.addEventListener('keyup', e => {
+   
     
 });
 
 
-const checkEmail = () => {
-    console.log(email.innerHTML)
-};
+submitBtn.addEventListener('click', e => {
+    checkCreditCard(cardNum, zipCode, cvv);
+   /* if (!submitted) {
+
+        basicError.style.display = "block";
+
+        emailError.style.display = "block";
+
+        cardError.style.display = "block";
+
+        zipCodeError.style.display = "block";
+
+        cvvError.style.display = "block";
+
+        activitiesError.style.display = "block";
+    }
+    */
+});
 
 
+const checkCreditCard = (cardNum, zipCode, cvv) => {
+    // check if cardNum.value between 13 and 16 or empty
+    console.log(!isNaN(cardNum.value));
+    if (!isNaN(cardNum.value) && (cardNum.value.length >= 13 && cardNum.value.length <= 16)) {
+        cardError.style.display = "none";
+        cardNum.style.outline = "1px solid green";
+
+    } else {
+        cardError.style.display = "block";
+        cardNum.style.outline = "1px solid red";
+    }
+
+    if (!isNaN(zipCode.value) && zipCode.value.length == 5) {
+        zipCodeError.style.display = "none";
+        zipCode.style.border = "1px solid green";
+
+    } else {
+        zipCodeError.style.display = "block";
+        zipCode.style.border = "1px solid red";
+    }
+
+    if (!isNaN(cvv.value) && cvv.value.length == 3) {
+        cvvError.style.display = "none";
+        cvv.style.border = "1px solid green";
+
+    } else {
+        cvvError.style.display = "block";
+        cvv.style.border = "1px solid red";
+    }
+    };
